df55a44cf8b5c0af0d162ab1aa66c51d257a829e
stickyfingers489 <mutuadennis56@gmai.com>
2022-05-09 21:22:56 +0300

    Initial Commit

---
+++ VideoPlayer/Components/QueryTag.swift
@@ -0,0 +1,20 @@
+//
+//  QueryTag.swift
+//  VideoPlayer
+//
+//  Created by stickerfinger489 on 09/05/2022.
+//
+
+import SwiftUI
+
+struct QueryTag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QueryTag_Previews: PreviewProvider {
+    static var previews: some View {
+        QueryTag()
+    }
+}
---
+++ VideoPlayer/Components/VideoCard.swift
@@ -0,0 +1,20 @@
+//
+//  VideoCard.swift
+//  VideoPlayer
+//
+//  Created by stickerfinger489 on 09/05/2022.
+//
+
+import SwiftUI
+
+struct VideoCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoCard_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoCard()
+    }
+}

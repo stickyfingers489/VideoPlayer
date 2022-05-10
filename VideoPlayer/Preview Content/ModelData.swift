365403fd5a1faf3bf6fc59966873176744b4ecf7
stickyfingers489 <mutuadennis56@gmai.com>
2022-05-10 20:13:18 +0300

    Third Commit

---
+++ VideoPlayer/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  VideoPlayer
+//
+//  Created by stickerfinger489 on 09/05/2022.
+//
+
+import Foundation
---
+++ VideoPlayer/VideoView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoView.swift
+//  VideoPlayer
+//
+//  Created by stickerfinger489 on 09/05/2022.
+//
+
+import SwiftUI
+
+struct VideoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoView()
+    }
+}
